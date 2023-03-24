a40f03cbd8ea7bbbe9b4bb7ed924b09362e015a2
KHS <ewoking777@gmail.com>
2023-03-24 22:36:04 +0900

    숫자야구게임 - 문자열 계산기 Test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,8 +3,6 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
