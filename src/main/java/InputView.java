072e682750071a4cc15b0f543c0d69829abf3cc6
KHS <ewoking777@gmail.com>
2023-03-25 21:01:32 +0900

    refactor: 2. 사용자 3자리 숫자 입력

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -1,14 +1,9 @@
 import java.util.Scanner;
 
 public class InputView {
-
     Scanner sc = new Scanner(System.in);
 
-    int[] inputNum() {
-        int[] arr = new int[3];
-        arr[0] = sc.nextInt();
-        arr[1] = sc.nextInt();
-        arr[2] = sc.nextInt();
-        return arr;
+    String inputNum() {
+        return sc.nextLine();
     }
 }
